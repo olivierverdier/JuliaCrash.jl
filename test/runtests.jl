c1e0a768863aa7bd3d94a8543629a401d09e51b8
Olivier Verdier <olivier.verdier@gmail.com>
2024-10-20 13:25:16 +0200

    Better test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,15 @@
-for M ∈ [42]
-    try
-        [] isa Vector{<:3}
-    catch e
-        if e isa TypeError
-            3
-        else
-            7
+using Test
+
+@testset begin
+    for M ∈ [42]
+        try
+            [] isa Vector{<:3}
+        catch e
+            if e isa TypeError
+                3
+            else
+                7
+            end
         end
     end
 end
-

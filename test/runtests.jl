e6602aa6c6670d60968a85cf243db9deac5b519b
Olivier Verdier <olivier.verdier@gmail.com>
2024-10-20 11:41:34 +0200

    Minimal crashing example
    https://github.com/JuliaLang/julia/issues/56248#issuecomment-2424219091

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,12 @@
-using Test
-using Manifolds
-
-
-
-@testset "Julia crash" for M in
-    [
-        Sphere(2),
-    ]
-    @test_throws TypeError [] isa Vector{<:Sphere(3)}
+for M ∈ [42]
+    try
+        [] isa Vector{<:3}
+    catch e
+        if e isa TypeError
+            3
+        else
+            7
+        end
+    end
 end
+
